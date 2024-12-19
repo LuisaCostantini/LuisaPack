995f28ccada0f5021628e04a59abf0517d9962b5
Luisa Costantini <126901462+LuisaCostantini@users.noreply.github.com>
2024-12-19 16:27:37 +0100

    Create complex.R

---
+++ R/complex.R
@@ -0,0 +1,13 @@
+complex <- function(x) {
+  if (x>0) {
+  print("this is a positive number")
+  }
+  else if (x<0) {
+    print("this is a negative number") 
+  } 
+  else { 
+    print("this is just zero! nor positive nor negative! Have you studied maths at school?")
+    }
+  } 
+
+
